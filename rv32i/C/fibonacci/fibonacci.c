f2341f5b8de315405072f9c4a8f19016fa22cb2a
PebPeb <67885698+PebPeb@users.noreply.github.com>
2023-07-21 22:52:27 -0400

    Fibonacci C implementation

---
+++ rv32i/C/fibonacci/fibonacci.c
@@ -0,0 +1,18 @@
+
+int main() {
+  int *results = 0x000000FC;
+
+  int x = 3;
+
+  int current = 0;
+  int next = 1;
+  int temp = 0;
+  while (x < 0) {
+    temp = current + next;
+    current = next;
+    next = temp;
+    x--;
+  }
+
+  *results = current;
+}
